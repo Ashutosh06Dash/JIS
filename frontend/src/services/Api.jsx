9605e4a63c700ec66357892a4b5f75c8771a5fc1
Ashutosh Dash <ashutoshdash2613@gmail.com>
2025-04-07 22:03:45 +0530

    backend-usl-added

--- frontend/src/services/Api.jsx
+++ frontend/src/services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // update with your backend URL
+  baseURL: "https://jis-backend-d6di.onrender.com/api", // update with your backend URL
 });
 
 // Add request interceptor for debugging
